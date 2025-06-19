f6f4f6363f08ebe9f7fd736a209c0968962b7f88
TRUYEN97 <samtruyen60@gmail.com>
2025-06-19 16:18:38 +0700

    first commit

---
+++ AppUtil/Common/Util.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AppUtil.Common
+{
+    public class Util
+    {
+        public static string FindGroup(string str, string regex)
+        {
+            if (!string.IsNullOrEmpty(str) && !string.IsNullOrEmpty(regex))
+            {
+                Match match = Regex.Match(str, regex);
+                if (match.Success)
+                {
+                    return match.Groups[1].Value;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ AppUtil/Errorcodemapper/ErrorCodeMapperConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using AppUtil.Service;
+
+namespace AppUtil.ErrorCodeMapper
+{
+    public class ErrorCodeMapperConfig
+    {
+        private string _station;
+        private string _product;
+        private string _remoteDir;
+
+        public ErrorCodeMapperConfig()
+        {
+            SftpConfig = new SftpConfig();
+            Product = null;
+            Station = null;
+        }
+        public SftpConfig _sftpConfig;
+        public SftpConfig SftpConfig { get => _sftpConfig; set { if (value != null) _sftpConfig = value; } }
+
+        public string RemoteDir { get => _remoteDir; set => _remoteDir = string.IsNullOrWhiteSpace(value) ? "ErrorCode" : value.Trim().ToUpper(); }
+        public string Product { get => _product; set => _product = string.IsNullOrWhiteSpace(value) ? "PRODUCT" : value.Trim().ToUpper(); }
+        public string Station { get => _station; set => _station = string.IsNullOrWhiteSpace(value) ? "STATION" : value.Trim().ToUpper(); }
+        public string RemoteNewFilePath => Path.Combine(RemoteDir, Product, Station, "newErrorCodes.csv");
+        public string RemoteFilePath => Path.Combine(RemoteDir, Product, Station, "ErrorCodes.csv");
+    }
+}
